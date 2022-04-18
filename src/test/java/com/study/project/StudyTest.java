84fcc80688fe1e62df7cece27648067dd823ad5b
DashaVeleva <dashaveleva@yandex.ru>
2022-04-18 22:04:18 +0300

    commitmessage

---
+++ src/test/java/com/study/project/StudyTest.java
@@ -0,0 +1,13 @@
+package com.study.project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StudyTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
