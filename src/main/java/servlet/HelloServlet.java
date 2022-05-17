cba1a87d30d1de398002c7248e059d4d1746f351
Darya <dashaveleva@yandex.ru>
2022-05-17 08:26:57 +0300

    changes in servlet string

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello Fucking World!";
+        message = "Hello World!";
     }
 
     @Override
