7416231ad524a5c1e221160aa4c32ddf7de8ecad
Darya <dashaveleva@yandex.ru>
2022-06-10 10:17:37 +0300

    change LoadTest

--- src/test/java/servlet/LoadTest.java
+++ src/test/java/servlet/LoadTest.java
@@ -1,20 +1,28 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class LoadTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         int threadCount = 20;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         for (int i = 0; i < 20; i++) {
             executorService.submit(() -> {
                 try {
-                    URLConnection connection = new URL("http://localhost:8080").openConnection();
+
+                    HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080").openConnection();
+                    connection.connect();
+
                     System.out.println("successful " + Thread.currentThread().getName());
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
@@ -22,6 +30,9 @@ public class LoadTest {
                 }
             });
         }
+
+        Thread.sleep(5000);
+        executorService.shutdown();
 
     }
 }
